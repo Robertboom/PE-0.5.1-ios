f53fbd9c699dc4f6a1fd13ba2af8629311518035
Robertboom <102368456+Robertboom@users.noreply.github.com>
2022-05-01 21:25:25 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -431,8 +431,7 @@ class PlayState extends MusicBeatState
 				var bg:BGSprite = new BGSprite('background ladders', -200, -290, 0.75, 0.75);
 				add(bg);
 
-				var stageFront:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
-				}
+				var bg:BGSprite = new BGSprite('stagefront', -650, 600, 0.9, 0.9);
 			case 'spooky': //Week 2
 				if(!ClientPrefs.lowQuality) {
 					halloweenBG = new BGSprite('halloween_bg', -200, -100, ['halloweem bg0', 'halloweem bg lightning strike']);
