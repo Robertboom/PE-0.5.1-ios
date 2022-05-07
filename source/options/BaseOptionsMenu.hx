85e0fd984d0c84ae8e682bbae3a400ad4abe2502
Robertboom <102368456+Robertboom@users.noreply.github.com>
2022-05-08 00:15:44 +0100

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -27,251 +27,156 @@ import Controls;
 
 using StringTools;
 
-class BaseOptionsMenu extends MusicBeatSubstate
-{
-	private var curOption:Option = null;
-	private var curSelected:Int = 0;
-	private var optionsArray:Array<Option>;
+class ControlsSubState extends MusicBeatSubstate {
+	private static var curSelected:Int = -1;
+	private static var curAlt:Bool = false;
+
+	private static var defaultKey:String = 'Reset to Default Keys';
+	private var bindLength:Int = 0;
+
+	var optionShit:Array<Dynamic> = [
+		['NOTES'],
+		['Left', 'note_left'],
+		['Down', 'note_down'],
+		['Up', 'note_up'],
+		['Right', 'note_right'],
+		[''],
+		['UI'],
+		['Left', 'ui_left'],
+		['Down', 'ui_down'],
+		['Up', 'ui_up'],
+		['Right', 'ui_right'],
+		[''],
+		['Reset', 'reset'],
+		['Accept', 'accept'],
+		['Back', 'back'],
+		['Pause', 'pause'],
+		[''],
+		['VOLUME'],
+		['Mute', 'volume_mute'],
+		['Up', 'volume_up'],
+		['Down', 'volume_down'],
+		[''],
+		['DEBUG'],
+		['Key 1', 'debug_1'],
+		['Key 2', 'debug_2']
+	];
 
 	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var checkboxGroup:FlxTypedGroup<CheckboxThingie>;
-	private var grpTexts:FlxTypedGroup<AttachedText>;
+	private var grpInputs:Array<AttachedText> = [];
+	private var grpInputsAlt:Array<AttachedText> = [];
+	var rebindingKey:Bool = false;
+	var nextAccept:Int = 5;
 
-	private var boyfriend:Character = null;
-	private var descBox:FlxSprite;
-	private var descText:FlxText;
-
-	public var title:String;
-	public var rpcTitle:String;
-
-	public function new()
-	{
+	public function new() {
 		super();
 
-		if(title == null) title = 'Options';
-		if(rpcTitle == null) rpcTitle = 'Options Menu';
-		
-		#if desktop
-		DiscordClient.changePresence(rpcTitle, null);
-		#end
-		
 		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.color = 0xFFea71fd;
-		bg.setGraphicSize(Std.int(bg.width * 1.1));
-		bg.updateHitbox();
 		bg.screenCenter();
 		bg.antialiasing = ClientPrefs.globalAntialiasing;
 		add(bg);
 
-		// avoids lagspikes while scrolling through menus!
 		grpOptions = new FlxTypedGroup<Alphabet>();
 		add(grpOptions);
 
-		grpTexts = new FlxTypedGroup<AttachedText>();
-		add(grpTexts);
+		optionShit.push(['']);
+		optionShit.push([defaultKey]);
 
-		checkboxGroup = new FlxTypedGroup<CheckboxThingie>();
-		add(checkboxGroup);
+		for (i in 0...optionShit.length) {
+			var isCentered:Bool = false;
+			var isDefaultKey:Bool = (optionShit[i][0] == defaultKey);
+			if(unselectableCheck(i, true)) {
+				isCentered = true;
+			}
 
-		descBox = new FlxSprite().makeGraphic(1, 1, FlxColor.BLACK);
-		descBox.alpha = 0.6;
-		add(descBox);
-
-		var titleText:Alphabet = new Alphabet(0, 0, title, true, false, 0, 0.6);
-		titleText.x += 60;
-		titleText.y += 40;
-		titleText.alpha = 0.4;
-		add(titleText);
-
-		descText = new FlxText(50, 600, 1180, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		descText.scrollFactor.set();
-		descText.borderSize = 2.4;
-		add(descText);
-
-		for (i in 0...optionsArray.length)
-		{
-			var optionText:Alphabet = new Alphabet(0, 70 * i, optionsArray[i].name, false, false);
+			var optionText:Alphabet = new Alphabet(0, (10 * i), optionShit[i][0], (!isCentered || isDefaultKey), false);
 			optionText.isMenuItem = true;
-			optionText.x += 300;
-			/*optionText.forceX = 300;
-			optionText.yMult = 90;*/
-			optionText.xAdd = 200;
+			if(isCentered) {
+				optionText.screenCenter(X);
+				optionText.forceX = optionText.x;
+				optionText.yAdd = -55;
+			} else {
+				optionText.forceX = 200;
+			}
+			optionText.yMult = 60;
 			optionText.targetY = i;
 			grpOptions.add(optionText);
 
-			if(optionsArray[i].type == 'bool') {
-				var checkbox:CheckboxThingie = new CheckboxThingie(optionText.x - 105, optionText.y, optionsArray[i].getValue() == true);
-				checkbox.sprTracker = optionText;
-				checkbox.ID = i;
-				checkboxGroup.add(checkbox);
-			} else {
-				optionText.x -= 80;
-				optionText.xAdd -= 80;
-				var valueText:AttachedText = new AttachedText('' + optionsArray[i].getValue(), optionText.width + 80);
-				valueText.sprTracker = optionText;
-				valueText.copyAlpha = true;
-				valueText.ID = i;
-				grpTexts.add(valueText);
-				optionsArray[i].setChild(valueText);
+			if(!isCentered) {
+				addBindTexts(optionText, i);
+				bindLength++;
+				if(curSelected < 0) curSelected = i;
 			}
-
-			if(optionsArray[i].showBoyfriend && boyfriend == null)
-			{
-				reloadBoyfriend();
-			}
-			updateTextFrom(optionsArray[i]);
 		}
-
 		changeSelection();
-		reloadCheckboxes();
-
-                #if html5
-		addVirtualPad(FULL, A_B_C);
-		#end
 	}
 
-	public function addOption(option:Option) {
-		if(optionsArray == null || optionsArray.length < 1) optionsArray = [];
-		optionsArray.push(option);
-	}
-
-	var nextAccept:Int = 5;
-	var holdTime:Float = 0;
-	var holdValue:Float = 0;
-	override function update(elapsed:Float)
-	{
-		if (controls.UI_UP_P)
-		{
-			changeSelection(-1);
-		}
-		if (controls.UI_DOWN_P)
-		{
-			changeSelection(1);
-		}
-
-		if (controls.BACK) {
-			FlxG.sound.play(Paths.sound('cancelMenu'));
-                        ClientPrefs.saveSettings();
-			#if html5
-			MusicBeatState.resetState();
-			#else
-			close();
-			#end
-		}
-
-		if(nextAccept <= 0)
-		{
-			var usesCheckbox = true;
-			if(curOption.type != 'bool')
-			{
-				usesCheckbox = false;
+	var leaving:Bool = false;
+	var bindingTime:Float = 0;
+	override function update(elapsed:Float) {
+		if(!rebindingKey) {
+			if (controls.UI_UP_P) {
+				changeSelection(-1);
+			}
+			if (controls.UI_DOWN_P) {
+				changeSelection(1);
+			}
+			if (controls.UI_LEFT_P || controls.UI_RIGHT_P) {
+				changeAlt();
 			}
 
-			if(usesCheckbox)
-			{
-				if(controls.ACCEPT)
-				{
-					FlxG.sound.play(Paths.sound('scrollMenu'));
-					curOption.setValue((curOption.getValue() == true) ? false : true);
-					curOption.change();
-					reloadCheckboxes();
-				}
-			} else {
-				if(controls.UI_LEFT || controls.UI_RIGHT) {
-					var pressed = (controls.UI_LEFT_P || controls.UI_RIGHT_P);
-					if(holdTime > 0.5 || pressed) {
-						if(pressed) {
-							var add:Dynamic = null;
-							if(curOption.type != 'string') {
-								add = controls.UI_LEFT ? -curOption.changeValue : curOption.changeValue;
-							}
-
-							switch(curOption.type)
-							{
-								case 'int' | 'float' | 'percent':
-									holdValue = curOption.getValue() + add;
-									if(holdValue < curOption.minValue) holdValue = curOption.minValue;
-									else if (holdValue > curOption.maxValue) holdValue = curOption.maxValue;
-
-									switch(curOption.type)
-									{
-										case 'int':
-											holdValue = Math.round(holdValue);
-											curOption.setValue(holdValue);
-
-										case 'float' | 'percent':
-											holdValue = FlxMath.roundDecimal(holdValue, curOption.decimals);
-											curOption.setValue(holdValue);
-									}
-
-								case 'string':
-									var num:Int = curOption.curOption; //lol
-									if(controls.UI_LEFT_P) --num;
-									else num++;
-
-									if(num < 0) {
-										num = curOption.options.length - 1;
-									} else if(num >= curOption.options.length) {
-										num = 0;
-									}
-
-									curOption.curOption = num;
-									curOption.setValue(curOption.options[num]); //lol
-									//trace(curOption.options[num]);
-							}
-							updateTextFrom(curOption);
-							curOption.change();
-							FlxG.sound.play(Paths.sound('scrollMenu'));
-						} else if(curOption.type != 'string') {
-							holdValue += curOption.scrollSpeed * elapsed * (controls.UI_LEFT ? -1 : 1);
-							if(holdValue < curOption.minValue) holdValue = curOption.minValue;
-							else if (holdValue > curOption.maxValue) holdValue = curOption.maxValue;
-
-							switch(curOption.type)
-							{
-								case 'int':
-									curOption.setValue(Math.round(holdValue));
-								
-								case 'float' | 'percent':
-									curOption.setValue(FlxMath.roundDecimal(holdValue, curOption.decimals));
-							}
-							updateTextFrom(curOption);
-							curOption.change();
-						}
-					}
-
-					if(curOption.type != 'string') {
-						holdTime += elapsed;
-					}
-				} else if(controls.UI_LEFT_R || controls.UI_RIGHT_R) {
-					clearHold();
-				}
-			}
-
-			if(controls.RESET#if html5 || _virtualpad.buttonC.justPressed #end)
-			{
-				for (i in 0...optionsArray.length)
-				{
-					var leOption:Option = optionsArray[i];
-					leOption.setValue(leOption.defaultValue);
-					if(leOption.type != 'bool')
-					{
-						if(leOption.type == 'string')
-						{
-							leOption.curOption = leOption.options.indexOf(leOption.getValue());
-						}
-						updateTextFrom(leOption);
-					}
-					leOption.change();
-				}
+			if (controls.BACK) {
+				ClientPrefs.reloadControls();
+				close();
 				FlxG.sound.play(Paths.sound('cancelMenu'));
-				reloadCheckboxes();
 			}
-		}
 
-		if(boyfriend != null && boyfriend.animation.curAnim.finished) {
-			boyfriend.dance();
+			if(controls.ACCEPT && nextAccept <= 0) {
+				if(optionShit[curSelected][0] == defaultKey) {
+					ClientPrefs.keyBinds = ClientPrefs.defaultKeys.copy();
+					reloadKeys();
+					changeSelection();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+				} else if(!unselectableCheck(curSelected)) {
+					bindingTime = 0;
+					rebindingKey = true;
+					if (curAlt) {
+						grpInputsAlt[getInputTextNum()].alpha = 0;
+					} else {
+						grpInputs[getInputTextNum()].alpha = 0;
+					}
+					FlxG.sound.play(Paths.sound('scrollMenu'));
+				}
+			}
+		} else {
+			var keyPressed:Int = FlxG.keys.firstJustPressed();
+			if (keyPressed > -1) {
+				var keysArray:Array<FlxKey> = ClientPrefs.keyBinds.get(optionShit[curSelected][1]);
+				keysArray[curAlt ? 1 : 0] = keyPressed;
+
+				var opposite:Int = (curAlt ? 0 : 1);
+				if(keysArray[opposite] == keysArray[1 - opposite]) {
+					keysArray[opposite] = NONE;
+				}
+				ClientPrefs.keyBinds.set(optionShit[curSelected][1], keysArray);
+
+				reloadKeys();
+				FlxG.sound.play(Paths.sound('confirmMenu'));
+				rebindingKey = false;
+			}
+
+			bindingTime += elapsed;
+			if(bindingTime > 5) {
+				if (curAlt) {
+					grpInputsAlt[curSelected].alpha = 1;
+				} else {
+					grpInputs[curSelected].alpha = 1;
+				}
+				FlxG.sound.play(Paths.sound('scrollMenu'));
+				rebindingKey = false;
+				bindingTime = 0;
+			}
 		}
 
 		if(nextAccept > 0) {
@@ -280,85 +185,162 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		super.update(elapsed);
 	}
 
-	function updateTextFrom(option:Option) {
-		var text:String = option.displayFormat;
-		var val:Dynamic = option.getValue();
-		if(option.type == 'percent') val *= 100;
-		var def:Dynamic = option.defaultValue;
-		option.text = text.replace('%v', val).replace('%d', def);
-	}
-
-	function clearHold()
-	{
-		if(holdTime > 0.5) {
-			FlxG.sound.play(Paths.sound('scrollMenu'));
+	function getInputTextNum() {
+		var num:Int = 0;
+		for (i in 0...curSelected) {
+			if(optionShit[i].length > 1) {
+				num++;
+			}
 		}
-		holdTime = 0;
+		return num;
 	}
 	
-	function changeSelection(change:Int = 0)
-	{
-		curSelected += change;
-		if (curSelected < 0)
-			curSelected = optionsArray.length - 1;
-		if (curSelected >= optionsArray.length)
-			curSelected = 0;
-
-		descText.text = optionsArray[curSelected].description;
-		descText.screenCenter(Y);
-		descText.y += 270;
+	function changeSelection(change:Int = 0) {
+		do {
+			curSelected += change;
+			if (curSelected < 0)
+				curSelected = optionShit.length - 1;
+			if (curSelected >= optionShit.length)
+				curSelected = 0;
+		} while(unselectableCheck(curSelected));
 
 		var bullShit:Int = 0;
+
+		for (i in 0...grpInputs.length) {
+			grpInputs[i].alpha = 0.6;
+		}
+		for (i in 0...grpInputsAlt.length) {
+			grpInputsAlt[i].alpha = 0.6;
+		}
 
 		for (item in grpOptions.members) {
 			item.targetY = bullShit - curSelected;
 			bullShit++;
 
-			item.alpha = 0.6;
-			if (item.targetY == 0) {
-				item.alpha = 1;
+			if(!unselectableCheck(bullShit-1)) {
+				item.alpha = 0.6;
+				if (item.targetY == 0) {
+					item.alpha = 1;
+					if(curAlt) {
+						for (i in 0...grpInputsAlt.length) {
+							if(grpInputsAlt[i].sprTracker == item) {
+								grpInputsAlt[i].alpha = 1;
+								break;
+							}
+						}
+					} else {
+						for (i in 0...grpInputs.length) {
+							if(grpInputs[i].sprTracker == item) {
+								grpInputs[i].alpha = 1;
+								break;
+							}
+						}
+					}
+				}
 			}
 		}
-		for (text in grpTexts) {
-			text.alpha = 0.6;
-			if(text.ID == curSelected) {
-				text.alpha = 1;
-			}
-		}
-
-		descBox.setPosition(descText.x - 10, descText.y - 10);
-		descBox.setGraphicSize(Std.int(descText.width + 20), Std.int(descText.height + 25));
-		descBox.updateHitbox();
-
-		if(boyfriend != null)
-		{
-			boyfriend.visible = optionsArray[curSelected].showBoyfriend;
-		}
-		curOption = optionsArray[curSelected]; //shorter lol
 		FlxG.sound.play(Paths.sound('scrollMenu'));
 	}
 
-	public function reloadBoyfriend()
-	{
-		var wasVisible:Bool = false;
-		if(boyfriend != null) {
-			wasVisible = boyfriend.visible;
-			boyfriend.kill();
-			remove(boyfriend);
-			boyfriend.destroy();
+	function changeAlt() {
+		curAlt = !curAlt;
+		for (i in 0...grpInputs.length) {
+			if(grpInputs[i].sprTracker == grpOptions.members[curSelected]) {
+				grpInputs[i].alpha = 0.6;
+				if(!curAlt) {
+					grpInputs[i].alpha = 1;
+				}
+				break;
+			}
 		}
-
-		boyfriend = new Character(840, 170, 'bf', true);
-		boyfriend.setGraphicSize(Std.int(boyfriend.width * 0.75));
-		boyfriend.updateHitbox();
-		boyfriend.dance();
-		insert(1, boyfriend);
-		boyfriend.visible = wasVisible;
+		for (i in 0...grpInputsAlt.length) {
+			if(grpInputsAlt[i].sprTracker == grpOptions.members[curSelected]) {
+				grpInputsAlt[i].alpha = 0.6;
+				if(curAlt) {
+					grpInputsAlt[i].alpha = 1;
+				}
+				break;
+			}
+		}
+		FlxG.sound.play(Paths.sound('scrollMenu'));
 	}
 
-	function reloadCheckboxes() {
-		for (checkbox in checkboxGroup) {
-			checkbox.daValue = (optionsArray[checkbox.ID].getValue() == true);
+	private function unselectableCheck(num:Int, ?checkDefaultKey:Bool = false):Bool {
+		if(optionShit[num][0] == defaultKey) {
+			return checkDefaultKey;
+		}
+		return optionShit[num].length < 2 && optionShit[num][0] != defaultKey;
+	}
+
+	private function addBindTexts(optionText:Alphabet, num:Int) {
+		var keys:Array<Dynamic> = ClientPrefs.keyBinds.get(optionShit[num][1]);
+		var text1 = new AttachedText(InputFormatter.getKeyName(keys[0]), 400, -55);
+		text1.setPosition(optionText.x + 400, optionText.y - 55);
+		text1.sprTracker = optionText;
+		grpInputs.push(text1);
+		add(text1);
+
+		var text2 = new AttachedText(InputFormatter.getKeyName(keys[1]), 650, -55);
+		text2.setPosition(optionText.x + 650, optionText.y - 55);
+		text2.sprTracker = optionText;
+		grpInputsAlt.push(text2);
+		add(text2);
+	}
+
+	function reloadKeys() {
+		while(grpInputs.length > 0) {
+			var item:AttachedText = grpInputs[0];
+			item.kill();
+			grpInputs.remove(item);
+			item.destroy();
+		}
+		while(grpInputsAlt.length > 0) {
+			var item:AttachedText = grpInputsAlt[0];
+			item.kill();
+			grpInputsAlt.remove(item);
+			item.destroy();
+		}
+
+		trace('Reloaded keys: ' + ClientPrefs.keyBinds);
+
+		for (i in 0...grpOptions.length) {
+			if(!unselectableCheck(i, true)) {
+				addBindTexts(grpOptions.members[i], i);
+			}
+		}
+
+
+		var bullShit:Int = 0;
+		for (i in 0...grpInputs.length) {
+			grpInputs[i].alpha = 0.6;
+		}
+		for (i in 0...grpInputsAlt.length) {
+			grpInputsAlt[i].alpha = 0.6;
+		}
+
+		for (item in grpOptions.members) {
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+
+			if(!unselectableCheck(bullShit-1)) {
+				item.alpha = 0.6;
+				if (item.targetY == 0) {
+					item.alpha = 1;
+					if(curAlt) {
+						for (i in 0...grpInputsAlt.length) {
+							if(grpInputsAlt[i].sprTracker == item) {
+								grpInputsAlt[i].alpha = 1;
+							}
+						}
+					} else {
+						for (i in 0...grpInputs.length) {
+							if(grpInputs[i].sprTracker == item) {
+								grpInputs[i].alpha = 1;
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
