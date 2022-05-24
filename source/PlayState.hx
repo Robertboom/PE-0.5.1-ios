ed33eaf5dbc6f16ffb7b187204ca2ad977951a2b
Robertboom <102368456+Robertboom@users.noreply.github.com>
2022-05-24 13:58:25 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3794,21 +3794,6 @@ class PlayState extends MusicBeatState
 					note.destroy();
 				}
 				return;
-				case 'sword note': //sword
-						if(boyfriend.animation.getByName('hurt') != null) {
-							boyfriend.playAnim('hurt', true);
-							boyfriend.specialAnim = true;
-						}
-				}
-				
-				note.wasGoodHit = false;
-				if (!note.isSustainNote)
-				{
-					note.kill();
-					notes.remove(note, true);
-					note.destroy();
-				}
-				return;
 			}
 
 			if (!note.isSustainNote)
