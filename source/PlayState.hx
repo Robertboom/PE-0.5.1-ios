b69a058c94ae4fad152cd035ef94ac2983e81dda
Robertboom <102368456+Robertboom@users.noreply.github.com>
2022-10-06 10:31:08 +0100

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -407,13 +407,12 @@ class PlayState extends MusicBeatState
 				add(bg);
 				
 				var grain:FlxSprite = new FlxSprite(repositionShit - 380, -800);
-				grain.frames = Paths.getPackerAtlas('images/Grain');
+				grain.frames = Paths.getPackerAtlas('Grain');
 				grain.animation.add('grains 1', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18], 12);
 				grain.animation.play('grains 1');
 				grain.scrollFactor.set(1, 1);
 				add(grain);
 				grain.antialiasing = false;
-
 				}
 
 			case 'spooky': //Week 2
