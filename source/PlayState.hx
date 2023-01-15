855390cdf0545ba3a2466f9a820ea4a8cbff6fef
Robertboom <102368456+Robertboom@users.noreply.github.com>
2023-01-15 15:26:16 +0000

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2255,18 +2255,18 @@ class PlayState extends MusicBeatState
 		else
 		
 		if (healthBar.percent < 80)
-			iconP1.animation.curAnim.curFrame = 2;
-		else
 			iconP1.animation.curAnim.curFrame = 0;
+		else
+			iconP1.animation.curAnim.curFrame = 2;
 
 		if (healthBar.percent > 80)
 			iconP2.animation.curAnim.curFrame = 1;
 		else
 		
 		if (healthBar.percent > 20)
-			iconP2.animation.curAnim.curFrame = 2;
-		else
 			iconP2.animation.curAnim.curFrame = 0;
+		else
+			iconP2.animation.curAnim.curFrame = 2;
 
 		if (FlxG.keys.anyJustPressed(debugKeysCharacter) && !endingSong && !inCutscene) {
 			persistentUpdate = false;
